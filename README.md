a5555bce578c9fa4156356ff11f5c242e4f0506f
ankanadas <aaankyyy@gmail.com>
2022-08-21 21:14:12 -0400

    adding screenshots

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Shopping Cart with React
+
 1. Home Page -> add product to cart, remove from cart
+
+![](src/HomePage.png)
+
 2. Cart page -> display product added to cart, remove from cart and display total amount
+
+![](src/CartPage.png)
--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import React, { useState } from 'react'
+
 
 function App() {
+  const [cart, setCart] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+
+    
+     <Header cart={cart} setCart={setCart}/>
+      <Routes>
+        <Route path = '/' element={<Home cart={cart} setCart={setCart}/>} />
+        <Route path = 'cart' element={<Cart cart={cart} setCart={setCart}/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
